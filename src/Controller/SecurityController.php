5b092a04d477a8cba07ea7b0a3f81cc7530c59df
crepefeu <esteban.rodriguez@live.fr>
2024-12-21 22:25:05 +0100

    Update security config

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,11 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils, CsrfTokenManagerInterface $csrfTokenManager): Response
     {
         if ($this->getUser()) {
             return $this->redirectToRoute('app_restaurant_index');
@@ -23,7 +24,8 @@ class SecurityController extends AbstractController
 
         return $this->render('security/login.html.twig', [
             'last_username' => $lastUsername,
-            'error' => $error
+            'error' => $error,
+            'csrf_token' => $csrfTokenManager->getToken('authenticate')->getValue(),
         ]);
     }
 
